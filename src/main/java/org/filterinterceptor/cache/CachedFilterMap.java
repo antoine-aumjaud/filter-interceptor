fc88e7232e0ee18919d81a63cacc9b64d765452b
antoine-aumjaud <antoine@aumjaud.fr>
2012-12-10 08:12:12 +0100

    General: change javadoc

--- src/main/java/org/filterinterceptor/cache/CachedFilterMap.java
+++ src/main/java/org/filterinterceptor/cache/CachedFilterMap.java
@@ -1,6 +1,8 @@
 package org.filterinterceptor.cache;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.locks.Lock;
@@ -8,7 +10,10 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
- * Create a child of HashMap to manage lock
+ * Create a HashMap container to manage locks.
+ * <p>
+ * This is quicker for read access than use {@link Hashtable} or a
+ * {@link Collections#synchronizedMap}
  */
 public class CachedFilterMap {
 	/**
@@ -23,8 +28,10 @@ public class CachedFilterMap {
 	 */
 	private final Map<String, CachedFilter> data = new HashMap<String, CachedFilter>();
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Removes all of the mappings from this map
+	 * <p>
+	 * Use the write lock
 	 * 
 	 * @see java.util.Map#clear()
 	 */
@@ -37,9 +44,12 @@ public class CachedFilterMap {
 		}
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Returns a Set view of the keys contained in this map
+	 * <p>
+	 * Use the read lock
 	 * 
+	 * @return a set view of the keys contained in this map
 	 * @see java.util.Map#keySet()
 	 */
 	public Set<String> keySet() {
@@ -51,8 +61,16 @@ public class CachedFilterMap {
 		}
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Returns the value to which the specified key is mapped, or null if this
+	 * map contains no mapping for the key.
+	 * <p>
+	 * Use the read lock
+	 * 
+	 * @param key
+	 *            the key whose associated value is to be returned
+	 * @return the value to which the specified key is mapped, or null if this
+	 *         map contains no mapping for the key
 	 * 
 	 * @see java.util.Map#get(java.lang.Object)
 	 */
@@ -65,8 +83,17 @@ public class CachedFilterMap {
 		}
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Associates the specified value with the specified key in this map
+	 * <p>
+	 * Use the write lock
+	 * 
+	 * @param key
+	 *            key with which the specified value is to be associated
+	 * @param value
+	 *            value to be associated with the specified key
+	 * @return the previous value associated with key, or null if there was no
+	 *         mapping for key.
 	 * 
 	 * @see java.util.Map#put(java.lang.Object, java.lang.Object)
 	 */
