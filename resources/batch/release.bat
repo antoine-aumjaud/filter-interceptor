964c87adb98cd45e63278cec0b3798156c322fc1
antoine-aumjaud <antoine@aumjaud.fr>
2012-12-10 07:51:23 +0100

    Maven release

--- resources/batch/release.bat
+++ resources/batch/release.bat
@@ -1,6 +1,17 @@
-pushd ../..
+@echo off
 
-mvn release:prepare -P release-artifacts
-::release:perform  
+setlocal
+
+pushd %~dp0..\..
+
+echo.
+set /p PASSPHRASE="Enter the GPG passphrase: "
+echo.
+echo Run Maven Release in %cd%
+mvn release:prepare release:perform -Darguments=-Dgpg.passphrase=%PASSPHRASE%
 
 popd
+
+endlocal
+
+pause
